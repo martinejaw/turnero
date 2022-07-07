2ac889c136f66aaad1d08903665c87530ade6e31
martinjaworski <martin.jaworski@42mate.com>
2022-07-07 15:46:58 -0300

    feat: added user resource and create endpoint

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaModule } from './core/prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+
+const globalModules = [PrismaModule];
+
+const resourceModules = [UsersModule];
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [...globalModules, ...resourceModules],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

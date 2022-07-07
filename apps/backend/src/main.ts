57a91b6f360ed61724175ae187940bfd6420e07b
martinjaworski <martin.jaworski@42mate.com>
2022-07-07 15:46:35 -0300

    feat: added global pipe and swagger support

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,24 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Add validation pipe
+  app.useGlobalPipes(new ValidationPipe());
+
+  // API Docs
+  const config = new DocumentBuilder()
+    .setTitle('Turnero Application API')
+    .setDescription('The Turnero API description')
+    .setVersion('1.0')
+    .addTag('Turnero')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
